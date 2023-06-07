001b6659a71ef268d621a451e3532bede5f3b66f
Crowdbotics <team@crowdbotics.com>
2023-06-07 12:55:55 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/ru_engage_41326/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ru_engage_41326;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "ru_engage_41326";
+  }
+}
