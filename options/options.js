051727a6268243d38c2c3beced2ecbb030ad5193
Adrian Cameron <ajcam@docs.rutgers.edu>
2023-06-07 09:01:39 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "ru-engage-41326",
   url: "https://ru-engage-41326.botics.co",
   api: "https://ru-engage-41326.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
